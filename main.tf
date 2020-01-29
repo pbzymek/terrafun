c58e01fcd6f8cd466725451703647311c41354d4
Tomasz Krzywda <tomasz.krzywda@airhelp.com>
2020-01-29 15:51:32 +0100

    Remove obsolete reference to the default security group

--- main.tf
+++ main.tf
@@ -14,11 +14,6 @@ data "aws_subnet_ids" "all" {
   vpc_id = data.aws_vpc.default.id
 }
 
-data "aws_security_group" "default" {
-  vpc_id = data.aws_vpc.default.id
-  name   = "default"
-}
-
 module "elb_http_80_sg" {
   source = "terraform-aws-modules/security-group/aws//modules/http-80"
 
